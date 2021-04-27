12abbd45a0346caa5840503cac3ac26ab057cc2a
Duy Nguyen <duynb92@gmail.com>
2021-04-27 22:57:56 +0800

    Init project and cocoapods

---
+++ CsvToRealm/CsvToRealm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CsvToRealm
+//
+//  Created by Duy Nguyen on 4/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        parseCsv()
+    }
+
+    func parseCsv() {
+        let mapper = ObjectMapper(type: ElectionRecord.self)
+        let parser = CsvParser(mapper: mapper)
+        let datas = try! parser.parse(fileName: "1976-2020-president", delimiter: ",")
+        print(datas)
+        
+    }
+}
+
